f9f6c0205fe57e1d0d1b1086cbf966fc0f74ae7b
Alex <cas98@bk.ru>
2018-11-28 17:59:11 +0700

    early valera
    constructor has been added. I started to write setters. Need to complete setters and change them according technical task.

--- app/models/valera.rb
+++ app/models/valera.rb
@@ -1,2 +1,30 @@
 class Valera < ApplicationRecord
+    attr_reader :hp, :maxHp, :mp, :maxMp, :attack, :defense, :lucky, :money, :xp, :level, :outTime, :skill
+    attr_writer :maxHp, :maxMp, :level, :outTime
+
+    def initialize
+      @hp = 100
+      @maxHp = 100
+      @mp = 20
+      @maxMp = 20
+      @attack = 3
+      @defense = 3
+      @lucky = 3
+      @money = 500
+      @xp = 0
+      @level = 0
+      @outTime = nil
+      @skill = ''
+    end
+
+    def hp=(value)
+        @hp = value
+        end
+    end
+
+    def mp=(value)
+        @mp = value
+        end
+    end
+
 end
