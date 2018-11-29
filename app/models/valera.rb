64341cead41bb2c95eb73ebe7b0c0b2aab3eb221
Гриша Тарасенко <fle4er@yandex.ru>
2018-11-29 08:29:35 +0700

    Deleted some trash, test fixes

--- app/models/valera.rb
+++ app/models/valera.rb
@@ -1,42 +1,36 @@
 class Valera < ApplicationRecord
-    attr_reader :hp, :maxHp, :mp, :maxMp, :attack, :defense, :lucky, :money, :xp, :level, :outTime, :skill
-    attr_writer :maxHp, :maxMp, :level, :outTime
+  #def initialize
+    #@hp = 100
+    #@maxHp = 100
+    #@mp = 20
+    #@maxMp = 50
+    #@attack = 3
+    #@defense = 3
+    #@lucky = 3
+    #@money = 500
+    #@xp = 0
+    #@level = 0
+    #@outTime = nil
+    #@skill = ''
+  #end
 
-    def initialize
-      @hp = 100
-      @maxHp = 100
-      @mp = 20
-      @maxMp = 50
-      @attack = 3
-      @defense = 3
-      @lucky = 3
-      @money = 500
-      @xp = 0
-      @level = 0
-      @outTime = nil
-      @skill = ''
+  def hp=(value)
+    @hp = @hp > @maxHp ? @hp = @maxHp : @hp = value
+  end
+
+  def mp=(value)
+    case value
+    when value.negative?
+      @mp = 0
+    when 0 .. @maxMp
+      @mp = value
+    when @maxMp .. @maxMp * 1.3
+      @mp = @maxMp
+      @hp = @hp * (0.8 + @level / 1000)
+    else
+      @mp = 0
+      @hp = @hp * (0.8 + @level / 1000)	
+      @outTime = Time.now + 900
     end
-
-    def hp=(value)
-        @hp = value
-			@hp > @maxHp ? @hp = @maxHp : @hp = value
-        end
-        
-    end
-
-    def mp=(value)
-        @mp = value
-			if(@mp > @maxMp && @mp <= (@maxMp * 1.3)) 
-				@mp = @maxMp
-				@hp = @hp * (0.8 + @level / 1000)
-			end
-			if(@mp > @maxMp * 1.3)	
-				time = Time.new
-				@mp = 0
-				@hp = @hp * (0.8 + @level / 1000)	
-				@outTime = time + 900
-			end
-        end
-    end
-
+  end
 end
--- spec/models/valera_spec.rb
+++ spec/models/valera_spec.rb
@@ -3,47 +3,46 @@ require 'rails_helper'
 RSpec.describe Valera, type: :model do
   describe 'Valera model' do
 
-    before(:each) do
-      valera = Valera.new
-    end
-
     after do
       Timecop.return
     end
 
+    let(:valera) { Valera.new }
+
     it 'initializing with default values' do
-      expect(Valera.hp).to eq 100
-      expect(Valera.maxHp).to eq 100
-      expect(Valera.mp).to eq 20
-      expect(Valera.maxMp).to eq 50
-      expect(Valera.attack).to eq 3
-      expect(Valera.defense).to eq 3
-      expect(Valera.lucky).to eq 3
-      expect(Valera.money).to eq 500
-      expect(Valera.xp).to eq 0
-      expect(Valera.level).to eq 0
-      expect(Valera.outTime).to eq nil
-      expect(Valera.skill).to eq ''
+      expect(valera.hp).to eq 100
+      expect(valera.maxHp).to eq 100
+      expect(valera.mp).to eq 20
+      expect(valera.maxMp).to eq 50
+      expect(valera.attack).to eq 3
+      expect(valera.defense).to eq 3
+      expect(valera.lucky).to eq 3
+      expect(valera.money).to eq 500
+      expect(valera.xp).to eq 0
+      expect(valera.level).to eq 0
+      expect(valera.outTime).to eq nil
+      expect(valera.skill).to eq ''
     end
 
-	it 'setting hp greater than 100' do
-	  valera.hp = 101
-	  expect(valera.hp).to eq 100
-	end
-
-    it 'decrease hp to 0.8 + level / 1000, when have mp (maxMp; maxMp * 1.3]' do
-      valera.mp = valera.maxMp * 1.3
-      expect(Valera.hp).to eq valera.maxHp * (0.8 + level / 1000)
+    context 'health' do
+      it 'setting hp greater than 100' do
+        valera.hp = 101
+        expect(valera.hp).to eq 100
+      end
     end
 
-    it 'going out about 15 minutes, when mp > maxMp * 1.3' do
-      valera.mp = valera.maxMp * 2
-      start = Time.now.to_i
-      ending = Time.now.to_i + 5
-      expect(Valera.outTime.to_i).to be_between(start, ending)
-      t = Time.now + 15 * 60
-      Timecop.travel(t)
-      expect(Valera.outTime).to eq nil
+    context 'mana' do
+      it 'decrease hp to 0.8 + level / 1000, when have mp (maxMp; maxMp * 1.3]' do
+        valera.mp = valera.maxMp * 1.3
+        expect(valera.hp).to eq valera.maxHp * (0.8 + level / 1000)
+      end
+
+      it 'going out about 15 minutes, when mp > maxMp * 1.3' do
+        valera.mp = valera.maxMp * 2
+        start = Time.now.to_i
+        ending = Time.now.to_i + 5
+        expect(valera.outTime.to_i).to be_between(start, ending)
+      end
     end
   end
 end
