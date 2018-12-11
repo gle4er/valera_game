de6bd204e1985eeff9900d49bf8eee326b42fa22
Гриша Тарасенко <fle4er@yandex.ru>
2018-12-11 08:29:59 +0700

    Rework redirect

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
-      redirect_to :controller => 'valeras', :action => 'index', :user_id => current_user.id
+      redirect_to controller: 'valeras', action: 'index'
     end
   end
 
