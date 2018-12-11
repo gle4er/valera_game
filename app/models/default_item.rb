4afff5d5f694aa7c0e6d695ba6149e0794083cb8
Гриша Тарасенко <fle4er@yandex.ru>
2018-12-11 10:44:18 +0700

    Fixed seeding tables

--- app/models/default_item.rb
+++ app/models/default_item.rb
@@ -1,9 +1,2 @@
 class DefaultItem < ApplicationRecord
-  before_create do
-    self.attributes.each do |attr_name, attr_value|
-      if attr_value == nil
-        self.send(attr_name + '=', 0)
-      end
-    end
-  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,2 @@
 class Event < ApplicationRecord
-  before_create do
-    self.attributes.each do |attr_name, attr_value|
-      if attr_value == nil
-        self.send(attr_name + '=', 0)
-      end
-    end
-  end
 end
