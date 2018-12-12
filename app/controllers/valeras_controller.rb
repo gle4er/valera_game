40f2725e57b19790ab69f8b0d6d31a9d687f369e
ImNia <agnia-kimi1996@mail.ru>
2018-12-12 20:39:34 +0700

    Make a radio button

--- app/controllers/valeras_controller.rb
+++ app/controllers/valeras_controller.rb
@@ -2,13 +2,17 @@ class ValerasController < ApplicationController
   before_action :authenticate_user!
 
   def create
-
+    current_user.create_valera
     #current_user.valera.skill = ???
   end
 
+  def new
+  end
+
   def show
-    if current_user.valera.nil?
-      current_user.create_valera
+    @valera = current_user.valera
+    if @valera.nil? or @valera.skill = ''
+      redirect_to action: 'new'
     end
     @valera = current_user.valera
   end
