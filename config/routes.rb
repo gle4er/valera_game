b8ab0c2a54240677ad82502e846f75a6cf9afb0c
Тарасенко Григорий Германович <fle4er@yandex.ru>
2018-12-17 16:14:22 +0700

    Added items button in valera page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resource :valeras
+  resource :valeras do
+    resources :items
+  end
   resolve("Valera") { [:valeras] }
   root 'static_pages#home'
 end
