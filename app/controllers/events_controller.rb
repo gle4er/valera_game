554584ecc82802ba7ed3f23d8a993b4c9a7d2606
ImNia <agnia-kimi1996@mail.ru>
2018-12-17 20:40:48 +0700

    Make apply events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,17 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.find params["id"]
+    @stats = @event.stats
+    p @stats
+    @stats.each do |stat, value|
+      unless value.nil?
+        new_value = current_user.valera.send(stat.to_s)
+        new_value += value
+        current_user.valera.send(stat.to_s + '=', new_value)
+      end
+    end
+    current_user.valera.save
   end
 
 end
