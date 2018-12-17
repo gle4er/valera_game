ac713bc3b14d82ee5a67a44302110a1844d89372
Гриша Тарасенко <fle4er@yandex.ru>
2018-12-17 20:56:04 +0700

    Added buying items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,31 @@
 class ItemsController < ApplicationController
-  def show
+  before_action :authenticate_user!
+
+  before_action do |controller|
     @valera = current_user.valera
+  end
+
+  def show
     @items = @valera.items
   end
 
   def create
-    @valera = current_user.valera
     @item = Item.find params["id"]
+    if @valera.money < @item.money
+      flash[:notice] = "Not enough money"
+      redirect_to :show
+    end
+    flash[:notice] = "Item is added to inventory"
     @valera.money -= @item.money
     @valera.items << @item
+    @valera.save
+    redirect_to :show
+  end
+
+  def destroy
+    @item_id = params["id"]
+    @item = @valera.items[@item_id]
+    @valera.items.delete @item
   end
 
   def new
