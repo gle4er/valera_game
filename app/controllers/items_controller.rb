209f1e29897b99f54c6ce757b086d457a4c862e2
Тарасенко Григорий Германович <fle4er@yandex.ru>
2018-12-17 17:40:51 +0700

    Added initial buying support

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,18 @@
 class ItemsController < ApplicationController
+  def show
+    @valera = current_user.valera
+    @items = @valera.items
+  end
+
+  def create
+    @valera = current_user.valera
+    @item = Item.find params["item"]["id"]
+    @valera.money -= @item.money
+    @valera.items << @item
+  end
+
+  def new
+    @item_list = Item.all
+    @item = Item.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resource :valeras do
-    resources :items
+    resource :items
   end
   resolve("Valera") { [:valeras] }
+  resolve("Item") { [:items] }
   root 'static_pages#home'
 end
